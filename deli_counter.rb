5ded46dcb2946b133f4290b5db891a809e8c52e4
Joseph Frasier <joseph.frasier@flatironschool.com>
2018-10-02 21:46:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(position)
   if position == []
     puts "The line is currently empty."
   else 
-    puts "The line is currently: "
+    put "The line is currently: "
     position.each_with_index {|val, index| puts "#{index+1} #{val}"}
     
   end
