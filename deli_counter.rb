b2dbe3bbedcc97c87a1a0c5f6bbb09541a630b5c
Joseph Frasier <joseph.frasier@flatironschool.com>
2018-10-02 21:34:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,9 @@ katz_deli = []
 def line(position)
   if position == []
     puts "The line is currently empty."
-  else position.each { |x| puts x }   
+  else 
+    position.each { |x| puts x }
+    
   end
 end
 
